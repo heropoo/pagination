25ec82f68e87851c785cfa70df9aa308286b0bdb
heropoo <aiyouyou1000@163.com>
2020-02-12 16:49:19 +0800

    page

--- Pagination.php
+++ Pagination.php
@@ -1,50 +1,59 @@
 <?php
+
 namespace Moon\Pagination;
 
 /**
  * 分页类
- * @author TTT
+ * @author Heropoo
  * @date 2015-01-19
- * @lastModified 2018-06-12
+ * @lastModified 2020-02-12
  */
 class Pagination
 {
     protected $totalRows;   //总数据条数
-    protected $pageSize;   //页大小
-    protected $pageVar;   //分页中的get参数
+    protected $pageSize;    //页大小
+    protected $pageVar;     //分页中的get参数
     protected $page;        //当前页数
     protected $totalPage;   //总页数
-    protected $url;        //当前页面地址
     protected $showPages;   //显示页数
-    public function __construct($total, $pageSize = 10, $showPages = 5, $pageVar = 'page', $url = '')
+    protected $get = [];         //$_GET
+    protected $baseUrl;     //当前页面地址
+
+    public function __construct($total, $pageSize = 10, $showPages = 5, $pageVar = 'page', $baseUrl = null, $get = [])
     {
         $this->totalRows = $total;
         $this->pageSize = $pageSize;
         $this->showPages = $showPages;
         $this->pageVar = $pageVar;
-        $this->page = !empty($_GET[$this->pageVar]) ? (int)$_GET[$this->pageVar] : 1;
-        $this->totalPage = ceil($this->totalRows / $this->pageSize);
-        $this->url = empty($url) ? $this->getPageUrl() : $url;
-    }
-    
-    protected function getPageUrl()
-    {
-        //获取当前页面地址
-        $this->url = $_SERVER['PHP_SELF'];
-        unset($_GET['page']);
-        if (empty($_GET)) {
-            $this->url .= "?{$this->pageVar}=";
-        } else {
-            $this->url = '?' . http_build_query($_GET);
-            $this->url .= "&{$this->pageVar}=";
+
+        if (!empty($get)) {
+            $this->get = $get;
+        } else if (isset($_GET)) {
+            $this->get = $_GET;
         }
-        return $this->url;
+
+        $this->page = !empty($this->get[$this->pageVar]) ? (int)$this->get[$this->pageVar] : 1;
+        $this->totalPage = ceil($this->totalRows / $this->pageSize);
+        $this->baseUrl = is_null($baseUrl) ? $_SERVER['PHP_SELF'] : $baseUrl;
+    }
+
+    protected function makePageUrl($page)
+    {
+        $url = $this->baseUrl;
+        if (empty($this->get)) {
+            $url .= "?{$this->pageVar}=" . $page;
+        } else {
+            $url = '?' . http_build_query($this->get);
+            $url .= "&{$this->pageVar}=" . $page;
+        }
+        return $url;
     }
 
     /**
      * 获取当前页码
      */
-    public function getCurrentPage(){
+    public function getCurrentPage()
+    {
         return $this->page;
     }
 
@@ -55,6 +64,7 @@ class Pagination
     {
         return $this->totalPage;
     }
+
     /**
      * 获取分页 offset 参数
      */
@@ -62,6 +72,7 @@ class Pagination
     {
         return ($this->page - 1) * $this->pageSize;
     }
+
     /**
      * 获取分页 limit 参数
      */
@@ -69,11 +80,12 @@ class Pagination
     {
         return $this->pageSize;
     }
+
     public function getLimitString()
     {
         return $this->getOffset() . ',' . $this->getLimit();
     }
-    
+
     /**
      * 获取分页输出的HTML代码
      */
@@ -81,48 +93,53 @@ class Pagination
     {
         $beforePage = (($this->page - 1) > 0) ? ($this->page - 1) : 1;   //上一页
         $nextPage = (($this->page + 1) < $this->totalPage) ? ($this->page + 1) : $this->totalPage; //下一页
+
         $pageHtml = '<ul class="pagination">';
         if ($this->page == 1) {
             $pageHtml .= '<li class="disabled"><span>首页</span></li> ';
         } else {
-            $pageHtml .= '<li><a href="' . $this->url . '">首页</a></li>';
-            $pageHtml .= '<li><a href="' . $this->url . $beforePage . '"><<上一页</a></li> ';
+            $pageHtml .= '<li><a href="' . $this->makePageUrl(1) . '">首页</a></li>';
+            $pageHtml .= '<li><a href="' . $this->makePageUrl($beforePage) . '"><<上一页</a></li> ';
         }
+
         $half = ceil($this->showPages);
         $left = $this->page - $half > 0 ? $this->page - $half : 0;
         $right = $this->page + $half < $this->totalPage ? $this->page + $half : $this->totalPage;
-        if($left && ($this->page - $half) > 1){
-            $pageHtml .= '<li><a href="' . $this->url . '1">1</a></li> ';
-            if($this->page - $half > 2){
+        if ($left && ($this->page - $half) > 1) {
+            $pageHtml .= '<li><a href="' . $this->makePageUrl(1) . '">1</a></li> ';
+            if ($this->page - $half > 2) {
                 $pageHtml .= '<li class="disabled"><span>...</span></li> ';
             }
         }
+
         for ($i = 1; $i <= $this->totalPage; $i++) {
-            if($i < $left){
+            if ($i < $left) {
                 continue;
             }
-            if($i > $right){
+            if ($i > $right) {
                 continue;
             }
             if ($this->page == $i) {
                 $pageHtml .= '<li class="active"><span>' . $i . '</span></li> ';
             } else {
-                $pageHtml .= '<li><a href="' . $this->url . $i . '">' . $i . '</a></li> ';
+                $pageHtml .= '<li><a href="' . $this->makePageUrl($i) . '">' . $i . '</a></li> ';
             }
         }
-        if($right && ($this->page + $half) < $this->totalPage){
-            if($this->page + $half + 1 < $this->totalPage){
+        if ($right && ($this->page + $half) < $this->totalPage) {
+            if ($this->page + $half + 1 < $this->totalPage) {
                 $pageHtml .= '<li><span>...</span></li> ';
             }
-            $pageHtml .= '<li><a class="pageItem" href="' . $this->url .$this->totalPage.'">'.$this->totalPage.'</a></li> ';
+            $pageHtml .= '<li><a class="pageItem" href="' . $this->makePageUrl($this->totalPage) . '">' . $this->totalPage . '</a></li> ';
         }
+
         if ($this->page == $this->totalPage) {
             $pageHtml .= '<li class="disabled"><span>末页</span></li>';
         } else {
-            $pageHtml .= '<li><a class="nextPage" href="' . $this->url . $nextPage . '">下一页>></a></li> ';
-            $pageHtml .= '<li><a class="lastPage" href="' . $this->url . $this->totalPage . '">末页</a></li>';
+            $pageHtml .= '<li><a class="nextPage" href="' . $this->makePageUrl($nextPage) . '">下一页>></a></li> ';
+            $pageHtml .= '<li><a class="lastPage" href="' . $this->makePageUrl($this->totalPage) . '">末页</a></li>';
         }
-        $pageHtml.= '</ul>';
+
+        $pageHtml .= '</ul>';
         return $pageHtml;
     }
 }
